552646f42087cabcfe40d8a4eec16af4f0967279
Rafael Celente <rafaelmcelente@gmail.com>
2025-09-06 14:47:19 -0300

    feat: wsd scheduler

--- src/core/optimizers/lr_scheduler_utils.py
+++ src/core/optimizers/lr_scheduler_utils.py
@@ -20,7 +20,23 @@ def constant_lr(
 ) -> float:
     return 1.0
 
+def warmup_then_stable_then_decay_lr(
+    it: int,
+    num_iterations: int,
+    cooldown_frac: float,
+    warmup_frac: float,
+) -> float:
+    t = 1 - it / num_iterations # time remaining in training
+    assert 1 >= t > 0
+    if t < warmup_frac:
+        return t / warmup_frac
+    elif t < cooldown_frac:
+        return 1.0
+    else:
+        return 1.0 - (t - cooldown_frac) / (1 - cooldown_frac)
+
 LR_SCHEDULER_FUNCTION_MAPPING: dict[str, Callable] = {
     "stable_then_decay": stable_then_decay_lr,
     "constant": constant_lr,
+    "wsd": warmup_then_stable_then_decay_lr,
 }
